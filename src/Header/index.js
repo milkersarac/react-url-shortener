9a3bd79a9a53899ed027e6ee4a416ab7602cc714
Ilker Sarac <ilker.sarac@t2.com.tr>
2017-10-27 20:32:45 +0300

    header added.

--- src/Header/index.js
+++ src/Header/index.js
@@ -5,7 +5,8 @@ export default class Header extends Component {
   render() {
     return (
       <header className="app-header">
-        <h1 className="app-title">Welcome to React</h1>
+        <h1 className="app-title">Shooooort</h1>
+        <h2 className="app-subtitle">The link shortener with a long name</h2>
       </header>
     );
   }
