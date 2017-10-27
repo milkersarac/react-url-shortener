92f396d4eb6caaa009ecbb3d43ec0123c9dbda44
Ilker Sarac <ilker.sarac@t2.com.tr>
2017-10-27 16:22:24 +0300

    initial commit

---
+++ src/Header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="app-header">
+        <h1 className="app-title">Welcome to React</h1>
+      </header>
+    );
+  }
+}
