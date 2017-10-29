02370f160f5af188d857f83a290a6dcacae6be83
Ilker Sarac <ilker.sarac@t2.com.tr>
2017-10-29 11:13:12 +0300

    mid info bar added.

--- src/Shortener/MidInfoBar/index.js
+++ src/Shortener/MidInfoBar/index.js
@@ -5,8 +5,14 @@ export default class MidInfoBar extends Component {
 
   render() {
     return (
-      <div>
-        Mid Info Bar here
+      <div className="mid-info-bar">
+        <div className="info-text section-headings">
+          Previously shortened by you
+        </div>
+        <button className="clear-button normal-text"
+          onClick={() => console.log('clear history clicked.')}>
+          Clear history
+        </button>
       </div>
     );
   }
